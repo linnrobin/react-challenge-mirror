7ac4156aeaeeb6b22efb378249039baba4cc2853
robin widjaja <robinsalim@yahoo.com>
2020-04-28 20:54:02 +0700

    repair as per requested MovieCard components

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,21 +1,16 @@
-import React from 'react'
-import { Card } from 'react-bootstrap'
+import React from "react";
+import MovieCard from "./MovieCard";
 
 export default class MovieList extends React.Component {
-    render() {
-        const { movies } = this.props
+  render() {
+    const { movies } = this.props;
 
-        return (
-            <ul>
-                {movies.map(movie => (
-                    <Card className="mt-3" style={{ width: '22rem', borderRadius: '10px', boxShadow: "2px 3px 10px #9E9E9E"}} key={movie.id}>
-                        <Card.Img variant="top" src={'https://image.tmdb.org/t/p/w500'+movie.poster_path} alt={movie.title} style={{width:'22rem', borderRadius: '10px 10px 0 0' }}/>
-                        <Card.Body>
-                            <Card.Title>{movie.title ? movie.title : movie.name}</Card.Title>
-                        </Card.Body>
-                    </Card>
-                ))}
-            </ul>
-        )
-    }
+    return (
+      <ul>
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </ul>
+    );
+  }
 }
